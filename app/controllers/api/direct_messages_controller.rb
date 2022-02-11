1ac26bed4bf4a2225b8642e064cee82b9271e7a3
Brian <brianlin1590@gmail.com>
2022-02-11 17:54:41 -0500

    add dm controller

--- app/controllers/api/direct_messages_controller.rb
+++ app/controllers/api/direct_messages_controller.rb
@@ -1,17 +1,19 @@
 class Api::DirectMessagesController < ApplicationController
 
     def index
-
+        @dms = DirectMessage.all
+        render "api/direct_messages/index"
     end
 
     def show
-
+        @dm = DirectMessage.includes(:messages).find_by(id: params[:id])
+        render "api/direct_messages/show"
     end
 
     def create
         @dm = DirectMessage.new(dm_params)
         if @dm.save
-            render "api/channels/show"
+            render "api/direct_messages/show"
         else
             errors = @dm.errors.full_messages
             render json: errors, status: 422
@@ -19,7 +21,14 @@ class Api::DirectMessagesController < ApplicationController
     end
 
     def destroy
+        @dm = DirectMessage.find_by(id: params[:id])
 
+        if @dm && @dm.delete
+            @dms = DirectMessage.all
+            render "api/direct_messages/show"
+        else
+            render json: ["You don't have permission/dm doesn't exist"], status: 422
+        end
     end
 
 
