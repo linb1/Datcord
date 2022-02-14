b09fa09cf5406c1f7a520b5841fc2348cb43b3c5
Brian <brianlin1590@gmail.com>
2022-02-14 17:22:57 -0500

    add dm to reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -5,12 +5,14 @@ import serversReducer from "./servers_reducer";
 import channelsReducer from "./channels_reducer";
 import membershipsReducer from "./memberships_reducer"
 import messagesReducer from "./messages_reducer";
+import dmsReducer from "./dms_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     servers: serversReducer,
     channels: channelsReducer,
     messages: messagesReducer,
+    dms: dmsReducer,
     // memberships: membershipsReducer
 });
 
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from "redux";
 import sessionErrorsReducer from "./session_errors_reducer";
 import serverErrorsReducer from "./server_errors_reducer";
 import channelErrorsReducer from "./channel_errors_reducer";
+import dmErrorsReducer from "./dm_errors_reducer"
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
     server: serverErrorsReducer,
-    channel: channelErrorsReducer
+    channel: channelErrorsReducer,
+    dm: dmErrorsReducer,
 });
 
 export default errorsReducer;
